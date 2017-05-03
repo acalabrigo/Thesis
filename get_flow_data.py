8f5cca8c910384023b972a0e0f50bcd23a7fc137
Adam Calabrigo <calabrigo.a@gmail.com>
2017-05-03 16:24:23 -0700

    fixed this

--- get_flow_data.py
+++ get_flow_data.py
@@ -16,10 +16,11 @@ if len(sys.argv) > 1:
         p = subprocess.Popen(["sudo", "ovs-ofctl", "dump-flows", "s{0}".format(i)],
                              stdout=subprocess.PIPE)
         out = p.communicate()[0]
-        flows = len(out.split('\n')) - 1
+        flows = len([f for f in out.split('\n') if 'cookie' in f])
+        print('switch {0}: {1} flows'.format(i, flows))
         num_flows[i] = flows
         total_flows += flows
-    print('Avg. flows per switch: {0}'.format(total_flows / num_switches))
+    print('Avg. flows per switch: {0}'.format(total_flows / (num_switches + 1)))
 else:
     print("Usage: ./get_flow_data.py <num switches>")
     exit()
